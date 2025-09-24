d4916c715d6d92f6f5c2b819ba36edafbf227e30
MateoRZJO <m.rodriguezj23@uniandes.edu.co>
2025-09-24 22:40:10 +0000

    Cambios 7

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 var config = {
     // style: 'mapbox://styles/mapbox/streets-v12',
     // deja comentado para usar el estilo estándar de Mapbox
-    tokenDeAcceso: `mapbox://styles/mrzj/cmfvnmw39004j01qm76ul8q0e`, // Reemplaza con tu propio token de acceso
+    tokenDeAcceso: `pk.eyJ1IjoibXJ6aiIsImEiOiJjbWZ2bjcydXYwOWYzMnFvdzhpdDM0N283In0.9qqIKuL670OIyMRzo3_1_Q`, // Reemplaza con tu propio token de acceso
     mostrarMarcadores: true,
     markerColor: '#3FB1CE',
     //projection: 'equirectangular',
@@ -28,9 +28,9 @@ var config = {
             imagen: './assets/The Weeknd.jpg', // Enlace a primera imagen
             descripcion: 'Abel Makkonen Tesfaye es un cantante canadiense de orígenes etíopes el cual comenzó su carrera musical en 2009 a los 19 años de forma anónima en YouTube. Su música explora el escapismo, así como el romance o la melancolía, inspirado usualmente de sus propias experiencias personales. Su estilo es variado, caracterizándose por hacer una fusión de géneros como el Disco, R&B, Pop, New waveo el Rhythm.',
             ubicacion: {
-                center: [63.222,-72.944], // Coordenadas del primer lugar
-                zoom: 3.42, // Nivel de zoom
-                inclinacion: 60, // Inclinación de la cámara
+                center: [-68.944,63.222], // Coordenadas del primer lugar
+                zoom: 3.40, // Nivel de zoom
+                inclinacion: 10, // Inclinación de la cámara
                 bearing: 0 // Rotación de la cámara
             },
             animacionMapa: 'flyTo',
@@ -58,10 +58,10 @@ var config = {
             imagen: './assets/Pulp Fiction .webp',
             descripcion: 'Pulp Fiction es una película de culto de 1994, además de ser la primer película postmoderna de gran éxito. Conocida por su estilo único, narrativa no lineal, calidad técnica, escenas memorables y sus ingeniosos diálogos, todos estos aspectos sumados nos han dado una obra que ha trascendido las generaciones y se ha vuelto parte escencial de la cultura popular moderna.',
             ubicacion: {
-                center: [38.503,-86.113], // Coordenadas del segundo lugar
-                zoom: 4.18,
-                inclinacion: 60,
-                bearing: -43.2,
+                center: [-76.113,38.503], // Coordenadas del segundo lugar
+                zoom: 4.50,
+                inclinacion: 10,
+                bearing: 0.00,
                 // Controles adicionales del flyTo:
                 // Estas opciones controlan la curva de vuelo, haciendo que se mueva
                 // lentamente y se aleje casi totalmente antes
@@ -79,12 +79,12 @@ var config = {
             id: 'tercer-identificador',
             alineacion: 'left',
             oculto: false,
-            titulo: 'Mexico',
+            titulo: 'México',
             imagen: './assets/Los supergenios.webp',
             descripcion: 'Los supergenios de la Mesa Cudrada fue una serie de comedia situacional, en la cual los protagonistas leían las cartas de los televidentes y las contestaban de forma humorística, llevando usualmente a mostrar algún sketch. Esta obra fue de las primeras del futuramente famoso Roberto Gómez Bolaños, la cual le dejó empezar a hacerse un lugar en la televisión.',
             ubicacion: {
-                center: [23.450,-103.755],
-                zoom: 4.94,
+                center: [-110.755,23.450],
+                zoom: 5.11,
                 inclinacion: 8.01,
                 bearing: 0.00
             },
@@ -95,17 +95,17 @@ var config = {
             onChapterExit: []
         },
         {
-            id: 'segundo-identificador',
+            id: 'cuarto-identificador',
             alineacion: 'right',
             oculto: false,
-            titulo: 'Centro América', // Título del segundo lugar
-            imagen: './assets/Pulp Fiction .webp',
+            titulo: 'Centro América', 
+            imagen: './assets/Popol vuh.jpg',
             descripcion: 'El Popol Vuh es una recopilación de narraciones míticas, legendarias e históricas de el pueblo K’iche’, siendo así un libro sagrado en la cultura maya. El libro conserva gran parte de la sabiduría y de las tradiciones mayas en aspectos tales como religión, astrología, o mitología, lo cual le ha otorgado gran valor, tanto en un punto de vista histórico como espiritual. La versión bilingüe actual es un trabajo que conserva los escritos originales de un autor indígena anónimo.',
             ubicacion: {
-                center: [12.911,-82.304], // Coordenadas del segundo lugar
-                zoom: 5.68,
-                inclinacion: 60,
-                bearing: -43.2,
+                center: [-79.7,12.868], 
+                zoom: 6.13,
+                inclinacion: 90,
+                bearing: -0.2,
                 // Controles adicionales del flyTo:
                 // Estas opciones controlan la curva de vuelo, haciendo que se mueva
                 // lentamente y se aleje casi totalmente antes
@@ -120,14 +120,15 @@ var config = {
             onChapterExit: []
         },
         {
+            id: 'quinto-identificador',
             alineacion: 'right',
             oculto: false,
             titulo: 'Caribe',
             imagen: './assets/Bob Marley.webp', // Enlace a primera imagen
             descripcion: 'Bob Marley fue un músico y exponente más conocido, así como representante del reggae. Su carrera musical se divide en 2, la primer etapa con The Wailers y la segunda en Bob Marley & The Wailers. Su aporte a esparcir la musica jamaiquina, asi como el movimiento rastafari fue de tal nivel, que ahora son conocidas a nivel mundial.',
             ubicacion: {
-                center: [18.159,-65.095], // Coordenadas del primer lugar
-                zoom: 5.09, // Nivel de zoom
+                center: [-63.095,18.159], // Coordenadas del primer lugar
+                zoom: 5.49, // Nivel de zoom
                 inclinacion: 60, // Inclinación de la cámara
                 bearing: 0 // Rotación de la cámara
             },
@@ -149,14 +150,15 @@ var config = {
             ]
         },
         {
+            id: 'sexto-identificador',
             alineacion: 'left',
             oculto: false,
             titulo: 'Colombia',
-            imagen: './assets/Bob Marley.webp', // Enlace a primera imagen
+            imagen: './assets/El man es German.jpg', // Enlace a primera imagen
             descripcion: 'El man es Germán es una serie humorística que sigue las situaciones de vida diarias de su protagonista Germán Quintero junto a su familia y amigos, ofreciendo a los espectadores un vistazo de aspectos estereotípicos de los colombianos promedio como el rebusque o la solidaridad.',
             ubicacion: {
-                center: [4.391,-80.401], // Coordenadas del primer lugar
-                zoom: 4.24, // Nivel de zoom
+                center: [-80.401,4.391], // Coordenadas del primer lugar
+                zoom: 5.60, // Nivel de zoom
                 inclinacion: 60, // Inclinación de la cámara
                 bearing: 0 // Rotación de la cámara
             },
@@ -178,14 +180,15 @@ var config = {
             ]
         },
         {
+            id: 'septimo-identificador',
             alineacion: 'left', 
             oculto: false,
             titulo: 'Venezuela',
-            imagen: './assets/Bob Marley.webp', // Enlace a primera imagen
+            imagen: './assets/Canaima.jpg', // Enlace a primera imagen
             descripcion: 'Canaima es una novela ambientada en la zona homónima de Venezuela, principalmente en las minas de El Callao. La obra es una queja en contra del caudillismo, mostrándonos la lucha en la selva debido a las ansias de riqueza, dominio y poder.',
             ubicacion: {
-                center: [6.692,-68.465], // Coordenadas del primer lugar
-                zoom: 5.68, // Nivel de zoom
+                center: [-69.995,6.692], // Coordenadas del primer lugar
+                zoom: 5.98, // Nivel de zoom
                 inclinacion: 60, // Inclinación de la cámara
                 bearing: 0 // Rotación de la cámara
             },
@@ -207,15 +210,15 @@ var config = {
             ]
         },
         {
-            id: 'tercer-identificador',
+            id: 'octavo-identificador',
             alineacion: 'left',
             oculto: false,
             titulo: 'Brasil',
-            imagen: './assets/31 Minutos.jpg',
-            descripcion: 'Cidade de Deus',
+            imagen: './assets/Ciudad de dios.jpg',
+            descripcion: 'Cidade de Deus es la adaptacion en formato cinematografico del año 2002 de la novela homonima escrita por Paulo Lins en 1997. La historia se citua en la favela Ciudad de Deus en rio de Janeiro, siguiendo la vida de varias personas locales y su relacion con las pandillas y violencia, asi como intento de escapar de dicho entorno. Es una obra basada ligeramente en hechos reales que retrata de manera acertada la vida completamente rodeada de violencia que se presenta en la vida de los habitantes de esta favela, asi como los de muchas otras.',
             ubicacion: {
-                center: [-14.220,-63.868],
-                zoom: 3.76,
+                center: [-63.968,-14.220],
+                zoom: 4.23,
                 inclinacion: 8.01,
                 bearing: 0.00
             },
@@ -226,17 +229,17 @@ var config = {
             onChapterExit: []
         },
          {
-            id: 'segundo-identificador',
+            id: 'noveno-identificador',
             alineacion: 'right',
             oculto: false,
             titulo: 'Perú', // Título del segundo lugar
-            imagen: './assets/Despelote.jpg',
+            imagen: './assets/Eternidad.png',
             descripcion: 'Wiñaypacha es no solo una película peruana, sino que es la primer película completamente hablada en aymara. La película sigue a Willka y Phaxsi, una pareja de ancianos los cuales mantienen vivas sus costumbres y que después de la migración de su único hijo quedan completamente solos, con la esperanza de que este último algún día regrese.',
             ubicacion: {
-                center: [-9.786,-62.629], // Coordenadas del segundo lugar
-                zoom: 5.09,
+                center: [-70.98,-9.890], // Coordenadas del segundo lugar
+                zoom: 5.55,
                 inclinacion: 60,
-                bearing: -43.2,
+                bearing: 0.00,
                 // Controles adicionales del flyTo:
                 // Estas opciones controlan la curva de vuelo, haciendo que se mueva
                 // lentamente y se aleje casi totalmente antes
@@ -251,15 +254,15 @@ var config = {
             onChapterExit: []
         },
         {
-            id: 'segundo-identificador',
+            id: 'decimo-identificador',
             titulo: 'Ecuador', // Título del segundo lugar
             imagen: './assets/Despelote.jpg',
             descripcion: 'Despelote es un videojuego en el cual el jugador se pone en el papel de un niño de 8 años en las calles de Quito en la epoca de clasificaciones para el mundial de Corea/Japón, el jugador, literalmente, a través de jugar con un balón de football explora la ciudad y descubre la gran cantidad de historias que viven en ésta.',
             ubicacion: {
-                center: [-1.533,-83.382], // Coordenadas del segundo lugar
-                zoom: 6.11,
+                center: [-84.122,-1.533], // Coordenadas del segundo lugar
+                zoom: 6.61,
                 inclinacion: 60,
-                bearing: -43.2,
+                bearing: 0.00,
                 // Controles adicionales del flyTo:
                 // Estas opciones controlan la curva de vuelo, haciendo que se mueva
                 // lentamente y se aleje casi totalmente antes
@@ -274,15 +277,15 @@ var config = {
             onChapterExit: []
         },
         {
-            id: 'tercer-identificador',
+            id: 'onceavo-identificador',
             alineacion: 'right',
             oculto: false,
             titulo: 'Chile',
             imagen: './assets/31 Minutos.jpg',
             descripcion: '31 minutos es una serie de televisión, la cual parodia a los noticieros, especialmente a 60 minutos. La serie posee soterradas referencias a la realidad social de Chile, estando enfocada principalmente a simple vsita hacia los niños, pero siendo disfrutable por personas de todas las edades.',
             ubicacion: {
-                center: [-38.659,-50.733],
-                zoom: 3.73,
+                center: [-74.733,-38.659],
+                zoom: 4.1,
                 inclinacion: 8.01,
                 bearing: 0.00
             },
@@ -293,14 +296,14 @@ var config = {
             onChapterExit: []
         },
         {
-            id: 'tercer-identificador',
+            id: 'doceavo-identificador',
             alineacion: 'left',
             oculto: false,
             titulo: 'Bolivia',
-            imagen: './assets/31 Minutos.jpg',
+            imagen: './assets/Los deshabitados.webp',
             descripcion: 'Los deshabitados es una novela escrita y publicada por Marcelo Quiroga Santa Cruz, de hecho fue la única novela que fue publicada cuando todavía estaba vivo, así como su única novela terminada. Fue galardonada por la Fundación William Faulkner como la mejor novela iberoamericana, siendo así la única obra boliviana poseedora de tal premio. Adicionalmente, dicha fundación la describe como “la mejor obra hispanoamericana desde la segunda guerra mundial”.',
             ubicacion: {
-                center: [-16.374,-62.256],
+                center: [-67.256,-16.374],
                 zoom: 5.51,
                 inclinacion: 8.01,
                 bearing: 0.00
@@ -312,14 +315,14 @@ var config = {
             onChapterExit: []
         },
         {
-            id: 'tercer-identificador',
+            id: 'terceavo-identificador',
             alineacion: 'right',
             oculto: false,
             titulo: 'Paraguay',
-            imagen: './assets/31 Minutos.jpg',
+            imagen: './assets/Hamaca paraguaya movie.jpg',
             descripcion: 'Hamaca paraguaya es una película ambientada en el 14 de julio de 1935, al final de la guerra del Chaco. Después de la partida de Maximiliano, quien se fue a la guerra, sus padres ancianos Cándida y Ramón esperan con ansias su regreso, la madre con pensamientos funestos y el padre con optimismo.',
             ubicacion: {
-                center: [-23.491,-56.412],
+                center: [-56.412,-23.491],
                 zoom: 6.10,
                 inclinacion: 8.01,
                 bearing: 0.00
@@ -331,17 +334,17 @@ var config = {
             onChapterExit: []
         },
         {
-            id: 'segundo-identificador',
+            id: 'catorceavo-identificador',
             alineacion: 'right',
             oculto: false,
             titulo: 'Uruguay', // Título del segundo lugar
             imagen: './assets/Whisky.jpg',
-            descripcion: 'Whisky es una pelicula que mezcla drama con comedia .',
+            descripcion: 'Whisky es una pelicula la cual mezcla drama con comedia, mostrandonos así, desde un código entre lo absurdo y la melancolía, entre lo cotidiano y lo fabulesco, un retrato sutil del cómo la torpeza y las pequeñas miserias de los 3 personajes prinncipales, tan distintos entre sí, se van evidenciando mientras intentan disimular resentimientos y asperezas. Una historia contada a través de detalles pequeños y trivialidades.',
             ubicacion: {
-                center: [-32.585,-54.736], // Coordenadas del segundo lugar
+                center: [-54.736,-32.585], // Coordenadas del segundo lugar
                 zoom: 6.70,
                 inclinacion: 60,
-                bearing: -43.2,
+                bearing: 0.00,
                 // Controles adicionales del flyTo:
                 // Estas opciones controlan la curva de vuelo, haciendo que se mueva
                 // lentamente y se aleje casi totalmente antes
@@ -356,15 +359,15 @@ var config = {
             onChapterExit: []
         },
         {
-            id: 'tercer-identificador',
+            id: 'quinceavo-identificador',
             alineacion: 'left',
             oculto: false,
             titulo: 'Argentina',
             imagen: './assets/Mafalda.webp',
             descripcion: 'Mafalda fue creada por Quino (Joaquín Salvador Lavada Tejón) en 1962 como publicidad para la compañía de electrodomésticos Siam Di Tella, al final estas historietas no fueron aprobadas ni publicadas, por lo cual la primera aparición de Mafalda sería hasta 1964. Mafalda habla sobre temas sociales de varias índoles, todo desde la perspectiva de una niña muy inteligente y con un perspicaz sentido del humor que genera de forma rápida y directa una crítica a lo sociedad y una reflexión al espectador. Mafalda es no solo un ícono y referente en su país de origen, sino que es famosa en todo el mundo, ya que Mafalda no depende de ser una niña de clase media de Argentina de los años 60 para poder conectar con ella, sino que por el contrario, todos podemos comprender de lo que habla, su postura y disfrutar de leerla.',
             ubicacion: {
-                center: [-38.821,-52.926],
-                zoom: 3.88,
+                center: [-61.682,-39.670],
+                zoom: 4.28,
                 inclinacion: 8.01,
                 bearing: 0.00
             },
