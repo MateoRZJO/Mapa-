0477f1429023758126717f4d7ed318148e2a6cea
MateoRZJO <m.rodriguezj23@uniandes.edu.co>
2025-09-24 02:00:11 +0000

    Cambios 6

--- config.js
+++ config.js
@@ -24,9 +24,9 @@ var config = {
             id: 'slug-style-id',
             alineacion: 'right',
             oculto: false,
-            titulo: 'Canada',
+            titulo: 'Canadá',
             imagen: './assets/The Weeknd.jpg', // Enlace a primera imagen
-            descripcion: 'Abel Makkonen Tesfaye es un cantante canadiense de orígenes etíopes el cual comenzó su carrera musical en 2009 a loos 19 años de forma anonima en YouTube. Su música explora el escapismo, así como el romance o la melancolia, inspirado usualmente de sus propias experiencias personales. Su estilo es variado, caracterizandose por hacer una fusión de generos como el Disco, R&B, Pop, New waveo el Rhythm.',
+            descripcion: 'Abel Makkonen Tesfaye es un cantante canadiense de orígenes etíopes el cual comenzó su carrera musical en 2009 a los 19 años de forma anónima en YouTube. Su música explora el escapismo, así como el romance o la melancolía, inspirado usualmente de sus propias experiencias personales. Su estilo es variado, caracterizándose por hacer una fusión de géneros como el Disco, R&B, Pop, New waveo el Rhythm.',
             ubicacion: {
                 center: [63.222,-72.944], // Coordenadas del primer lugar
                 zoom: 3.42, // Nivel de zoom
@@ -56,7 +56,7 @@ var config = {
             oculto: false,
             titulo: 'Estados Unidos', // Título del segundo lugar
             imagen: './assets/Pulp Fiction .webp',
-            descripcion: 'Pulp Fiction es una pelicula de culto de 1994, ademas de ser la primer pelicula postmoderna de gran exito. Conocida por su estilo unico, narrativa no lineal, calidad tecnoica, memorabilidad y sus ingeniosos dialogos, los cuales sumados nos han dado una obra que ha trasendido las generaciones y se ha vuelto parte escencial de la cultura popular moderna',
+            descripcion: 'Pulp Fiction es una película de culto de 1994, además de ser la primer película postmoderna de gran éxito. Conocida por su estilo único, narrativa no lineal, calidad técnica, escenas memorables y sus ingeniosos diálogos, todos estos aspectos sumados nos han dado una obra que ha trascendido las generaciones y se ha vuelto parte escencial de la cultura popular moderna.',
             ubicacion: {
                 center: [38.503,-86.113], // Coordenadas del segundo lugar
                 zoom: 4.18,
@@ -81,7 +81,7 @@ var config = {
             oculto: false,
             titulo: 'Mexico',
             imagen: './assets/Los supergenios.webp',
-            descripcion: 'Los supergenios de la Mesa Cudrada fue una serie de comedia situacional, en la cual los 5 personaje principales leían las cartas de los televidentes y las contestaban de forma humorística, llevando usualmente a mostarar algun sketch.Esta obra fue de las primeras del futuramente famoso Roberto Gómez Bolaños, la cual le dejo empezar a hacerse un lugar en la televisión',
+            descripcion: 'Los supergenios de la Mesa Cudrada fue una serie de comedia situacional, en la cual los protagonistas leían las cartas de los televidentes y las contestaban de forma humorística, llevando usualmente a mostrar algún sketch. Esta obra fue de las primeras del futuramente famoso Roberto Gómez Bolaños, la cual le dejó empezar a hacerse un lugar en la televisión.',
             ubicacion: {
                 center: [23.450,-103.755],
                 zoom: 4.94,
@@ -100,7 +100,7 @@ var config = {
             oculto: false,
             titulo: 'Centro América', // Título del segundo lugar
             imagen: './assets/Pulp Fiction .webp',
-            descripcion: 'El Popol Vuh es una recopilación de narraciones míticas, legendarias e históricas de el pueblo K’iche’, siendo así un libro sagrado en la cultura maya. El libro conserva gran parte de la sabiduría y de las tradiciones mayas en aspectos tales como religión, astrología, o mitología, lo cual le ha otorgado gran valor, tanto en un punto de vista histórico como espiritual. La versión bilingüe actual es un trabajo que conserva los escritos originales de un autor indigena anonimo.',
+            descripcion: 'El Popol Vuh es una recopilación de narraciones míticas, legendarias e históricas de el pueblo K’iche’, siendo así un libro sagrado en la cultura maya. El libro conserva gran parte de la sabiduría y de las tradiciones mayas en aspectos tales como religión, astrología, o mitología, lo cual le ha otorgado gran valor, tanto en un punto de vista histórico como espiritual. La versión bilingüe actual es un trabajo que conserva los escritos originales de un autor indígena anónimo.',
             ubicacion: {
                 center: [12.911,-82.304], // Coordenadas del segundo lugar
                 zoom: 5.68,
@@ -124,7 +124,7 @@ var config = {
             oculto: false,
             titulo: 'Caribe',
             imagen: './assets/Bob Marley.webp', // Enlace a primera imagen
-            descripcion: 'Bob Marley fue un musico y exponente mas conocido, así como representante del reggae. Su carrera musical se divide en 2, la primer etapa con The Wailers y la segunda en Bob Marley & The Wailers. Su aporte a esparcir la musica jamaiquina, asi como el movimiento rastafari fue de tal nivel que ahora son conocidas a nivel mundial',
+            descripcion: 'Bob Marley fue un músico y exponente más conocido, así como representante del reggae. Su carrera musical se divide en 2, la primer etapa con The Wailers y la segunda en Bob Marley & The Wailers. Su aporte a esparcir la musica jamaiquina, asi como el movimiento rastafari fue de tal nivel, que ahora son conocidas a nivel mundial.',
             ubicacion: {
                 center: [18.159,-65.095], // Coordenadas del primer lugar
                 zoom: 5.09, // Nivel de zoom
@@ -182,7 +182,7 @@ var config = {
             oculto: false,
             titulo: 'Venezuela',
             imagen: './assets/Bob Marley.webp', // Enlace a primera imagen
-            descripcion: 'Canaima.',
+            descripcion: 'Canaima es una novela ambientada en la zona homónima de Venezuela, principalmente en las minas de El Callao. La obra es una queja en contra del caudillismo, mostrándonos la lucha en la selva debido a las ansias de riqueza, dominio y poder.',
             ubicacion: {
                 center: [6.692,-68.465], // Coordenadas del primer lugar
                 zoom: 5.68, // Nivel de zoom
@@ -231,7 +231,7 @@ var config = {
             oculto: false,
             titulo: 'Perú', // Título del segundo lugar
             imagen: './assets/Despelote.jpg',
-            descripcion: 'Wyñaypacha.',
+            descripcion: 'Wiñaypacha es no solo una película peruana, sino que es la primer película completamente hablada en aymara. La película sigue a Willka y Phaxsi, una pareja de ancianos los cuales mantienen vivas sus costumbres y que después de la migración de su único hijo quedan completamente solos, con la esperanza de que este último algún día regrese.',
             ubicacion: {
                 center: [-9.786,-62.629], // Coordenadas del segundo lugar
                 zoom: 5.09,
@@ -254,7 +254,7 @@ var config = {
             id: 'segundo-identificador',
             titulo: 'Ecuador', // Título del segundo lugar
             imagen: './assets/Despelote.jpg',
-            descripcion: 'Despelote.',
+            descripcion: 'Despelote es un videojuego en el cual el jugador se pone en el papel de un niño de 8 años en las calles de Quito en la epoca de clasificaciones para el mundial de Corea/Japón, el jugador, literalmente, a través de jugar con un balón de football explora la ciudad y descubre la gran cantidad de historias que viven en ésta.',
             ubicacion: {
                 center: [-1.533,-83.382], // Coordenadas del segundo lugar
                 zoom: 6.11,
@@ -279,7 +279,7 @@ var config = {
             oculto: false,
             titulo: 'Chile',
             imagen: './assets/31 Minutos.jpg',
-            descripcion: '31 minutos es una serie de televisión chilena, la cual parodia a los noticieros, especialmente a 60 minutos. La serie posee soterradas referencias a la realidad social de Chile.',
+            descripcion: '31 minutos es una serie de televisión, la cual parodia a los noticieros, especialmente a 60 minutos. La serie posee soterradas referencias a la realidad social de Chile, estando enfocada principalmente a simple vsita hacia los niños, pero siendo disfrutable por personas de todas las edades.',
             ubicacion: {
                 center: [-38.659,-50.733],
                 zoom: 3.73,
@@ -298,7 +298,7 @@ var config = {
             oculto: false,
             titulo: 'Bolivia',
             imagen: './assets/31 Minutos.jpg',
-            descripcion: 'Los deshabitados es una novela escrita y publicada por Marcelo Quiroga Santa Cruz, de hecho fue la única novela que fue publicada cuando todavía estaba vivo así como su única novela terminada. Fue galardonada por la Fundación William Faulkner como la mejor novela iberoamericana, siendo así la única obra boliviana poseedora de tal premio. Adicionalmente dicha fundación la describe como “la mejor obra hispanoamericana desde la segunda guerra mundial”.',
+            descripcion: 'Los deshabitados es una novela escrita y publicada por Marcelo Quiroga Santa Cruz, de hecho fue la única novela que fue publicada cuando todavía estaba vivo, así como su única novela terminada. Fue galardonada por la Fundación William Faulkner como la mejor novela iberoamericana, siendo así la única obra boliviana poseedora de tal premio. Adicionalmente, dicha fundación la describe como “la mejor obra hispanoamericana desde la segunda guerra mundial”.',
             ubicacion: {
                 center: [-16.374,-62.256],
                 zoom: 5.51,
@@ -317,7 +317,7 @@ var config = {
             oculto: false,
             titulo: 'Paraguay',
             imagen: './assets/31 Minutos.jpg',
-            descripcion: 'Hamaca paraguaya es una película ambientada en el 14 de julio de 1935, al final de la guerra del chaco.',
+            descripcion: 'Hamaca paraguaya es una película ambientada en el 14 de julio de 1935, al final de la guerra del Chaco. Después de la partida de Maximiliano, quien se fue a la guerra, sus padres ancianos Cándida y Ramón esperan con ansias su regreso, la madre con pensamientos funestos y el padre con optimismo.',
             ubicacion: {
                 center: [-23.491,-56.412],
                 zoom: 6.10,
@@ -336,7 +336,7 @@ var config = {
             oculto: false,
             titulo: 'Uruguay', // Título del segundo lugar
             imagen: './assets/Whisky.jpg',
-            descripcion: 'Whisky.',
+            descripcion: 'Whisky es una pelicula que mezcla drama con comedia .',
             ubicacion: {
                 center: [-32.585,-54.736], // Coordenadas del segundo lugar
                 zoom: 6.70,
@@ -361,7 +361,7 @@ var config = {
             oculto: false,
             titulo: 'Argentina',
             imagen: './assets/Mafalda.webp',
-            descripcion: 'Mafalda fue creada por Quino (Joaquín Salvador Lavada Tejón) en 1962 como método de publicidad para la compañía de electrodomésticos Siam Di Tella, al final las las historietas de Quino no fueron aprobadas y publicadas, por lo cual la primera aparición de Mafalda sería hasta 1964 en el semanario Primera Plana. Mafalda habla sobre temas sociales de varias índoles, todo desde la perspectiva de una niña muy inteligente y con un perspicaz sentido del humor que genera de forma rápida y directa una crítica a lo sociedad y una reflexión al espectador. Mafalda es no solo la historieta más exitosa y famosa de Argentina, sino de cualquier país hispano, siendo todo un icono y referente en su país de origen, así como famosa en todo el mundo, ya que Mafalda no depende de ser una niña clase media de Argentina de los años 60 para poder conectar con ella, sino que por el contrario todos podemos comprender de lo que habla, su postura y disfrutar de leerla.',
+            descripcion: 'Mafalda fue creada por Quino (Joaquín Salvador Lavada Tejón) en 1962 como publicidad para la compañía de electrodomésticos Siam Di Tella, al final estas historietas no fueron aprobadas ni publicadas, por lo cual la primera aparición de Mafalda sería hasta 1964. Mafalda habla sobre temas sociales de varias índoles, todo desde la perspectiva de una niña muy inteligente y con un perspicaz sentido del humor que genera de forma rápida y directa una crítica a lo sociedad y una reflexión al espectador. Mafalda es no solo un ícono y referente en su país de origen, sino que es famosa en todo el mundo, ya que Mafalda no depende de ser una niña de clase media de Argentina de los años 60 para poder conectar con ella, sino que por el contrario, todos podemos comprender de lo que habla, su postura y disfrutar de leerla.',
             ubicacion: {
                 center: [-38.821,-52.926],
                 zoom: 3.88,
